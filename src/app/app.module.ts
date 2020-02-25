bc290b38696791a0f9b700b38cce4f006c6bfb64
Fadejimi Adegbulugbe <adegbulugbefadejimi@gmail.com>
2020-02-25 15:28:04 -0500

    sidebars added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { SidebuttonsComponent } from './components/sidebuttons/sidebuttons.component';
+import { SbuttonComponent } from './components/sbutton/sbutton.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebuttonsComponent,
+    SbuttonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sidebuttons/sidebuttons.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebuttons',
+  templateUrl: './sidebuttons.component.html',
+  styleUrls: ['./sidebuttons.component.css']
+})
+export class SidebuttonsComponent implements OnInit {
+  currentPg:number=0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getPg():number{
+    return this.currentPg;
+  }
+
+  setPg(pg:number){
+    this.currentPg=pg;
+  }
+}
