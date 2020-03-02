57b96ccbb0f142a604d9c0db780e11729b44cb6e
sushrestha <sushrestha@altimetrik.com>
2020-03-02 18:21:51 +0000

    Revert "Merge branch 'dev_sujiv' into 'master'"
    This reverts merge request !22

--- src/app/modules/camera/threat/threat.component.ts
+++ src/app/modules/camera/threat/threat.component.ts
@@ -1,6 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Evnt} from '../../../models/evnt';
-import {formatDate} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-threat',
@@ -8,24 +6,20 @@ import {formatDate} from '@angular/common';
   styleUrls: ['./threat.component.css']
 })
 export class ThreatComponent implements OnInit {
-  @Input()threat: Evnt;
 
   public codec = 'H264';
   public format = 'QUICKTIME / MOV';
   public duration = '2 SEC';
   public frameRate = '25.0';
 
-  // public timestamp = '0';
-  // public label = 'MACHINE';
-  // public confidence = '98.9598549';
-  // public instances = '';
+  public timestamp = '0';
+  public label = 'MACHINE';
+  public confidence = '98.9598549';
+  public instances = '';
 
   constructor() { }
 
   ngOnInit(): void {
-    // const d: Date = formatDate(Date.parse(this.threat.timestamp), 'MM/dd/yyyy', 'en');
-    const d: Date = new Date(this.threat.timestamp);
-    console.log(new Date(this.threat.timestamp));
   }
 
 }
