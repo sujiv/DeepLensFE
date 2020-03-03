176c2aa81956c644f22842f8f0b9326bceff1665
Sujiv Shrestha <46515284+sujiv@users.noreply.github.com>
2020-03-03 00:34:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/assets'));
+app.use(express.static('/dist/DeepLensFE'));
 
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join('/dist/DeepLensFE/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
