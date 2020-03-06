f3759ddac30f9627c422214d359b77ae4da6d366
Sujiv Shrestha <46515284+sujiv@users.noreply.github.com>
2020-03-06 02:20:57 -0500

    routing path params

--- src/app/modules/history/main-history/main-history.component.ts
+++ src/app/modules/history/main-history/main-history.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FilterHistoryComponent} from './filter-history/filter-history.component';
 import {HistoryBarChartComponent} from './history-bar-chart/history-bar-chart.component';
 import {HistoricalThreatListComponent} from './historical-threat-list/historical-threat-list.component';
+import {CameraZoneService} from '../../../services/camera-zone.service';
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-main-history',
   templateUrl: './main-history.component.html',
@@ -10,10 +12,21 @@ import {HistoricalThreatListComponent} from './historical-threat-list/historical
 export class MainHistoryComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(private cameraZoneService: CameraZoneService, private route: ActivatedRoute) { }
 
 
   ngOnInit(): void {
+    this.route.params.subscribe(p => {
+      if (p.id !== undefined) {
+        this.cameraZoneService.setCurrentPlantId(p.pid);
+      }
+      if (p.zid !== undefined) {
+        this.cameraZoneService.setCurrentZoneId(p.zid);
+      }
+      if (p.cid !== undefined) {
+        this.cameraZoneService.setCurrentCameraId(p.cid);
+      }
+    });
   }
 
 }
